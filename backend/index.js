b52d083ba877f152a503ac432cf553f14ab2e560
Debajyoti Pandit <codeninja0812@gmail.com>
2024-07-11 00:46:41 +0530

    Make ready for production

--- backend/index.js
+++ backend/index.js
@@ -4,22 +4,35 @@ import express from "express";
 //connection database
 import { connect } from "./database/connect.js";
 import userRouter from "./routes/user.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 
 //database conection
 connect();
 
-//one api route
-app.get("/", (req, res) => {
-  res.json({
-    message: "This is going to production",
-    status: 200,
-  });
-});
-
 //get user route
 app.use("/api/users", userRouter);
+
+// ------Deployment--------
+if (process.env.NODE_ENV == "production") {
+  //serve the static page
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.json({
+      message: "This is going to production",
+      status: 200,
+    });
+  });
+}
+// ------Deployment--------
 
 //server from app
 app.listen(process.env.PORT, () => {
